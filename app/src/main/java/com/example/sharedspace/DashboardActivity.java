bbd97f55e8c20edce0eb13037daf88b7d56156d8
junwei <limjunwei567@gmail.com>
2020-11-03 00:05:10 +0800

    update menu

---
+++ app/src/main/java/com/example/sharedspace/DashboardActivity.java
@@ -0,0 +1,116 @@
+package com.example.sharedspace;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    // firebase auth
+    FirebaseAuth firebaseAuth;
+
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+
+        // home fragment transaction (default on start)
+        actionBar.setTitle("Home");
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction hft = getSupportFragmentManager().beginTransaction();
+        hft.replace(R.id.container, homeFragment, "");
+        hft.commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            // home fragment transaction
+                            actionBar.setTitle("Home");
+                            HomeFragment homeFragment = new HomeFragment();
+                            FragmentTransaction hft = getSupportFragmentManager().beginTransaction();
+                            hft.replace(R.id.content, homeFragment, "");
+                            hft.commit();
+                            return true;
+                        case R.id.nav_profile:
+                            // profile fragment transaction
+                            actionBar.setTitle("Profile");
+                            HomeFragment profileFragment = new HomeFragment();
+                            FragmentTransaction pft = getSupportFragmentManager().beginTransaction();
+                            pft.replace(R.id.content, profileFragment, "");
+                            pft.commit();
+                            return true;
+                        case R.id.nav_friends:
+                            // users fragment transaction
+                            actionBar.setTitle("Friends");
+                            HomeFragment friendFragment = new HomeFragment();
+                            FragmentTransaction fft = getSupportFragmentManager().beginTransaction();
+                            fft.replace(R.id.content, friendFragment, "");
+                            fft.commit();
+                            return true;
+                    }
+                    return false;
+                }
+            };
+
+    private void checkUserStatus() {
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            // user is signed in
+//            mProfileTv.setText(user.getEmail());
+        } else {
+            // go to main activity
+            startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
+    }
+
+    //
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
